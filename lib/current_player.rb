2ad3a75a64c02ad905a5d28a156069f24b080299
happy-kernel-6078 <wolf_vii@yahoo.com>
2020-11-02 03:10:36 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   counter = 0
   board.each do |number|
     #do something to each element #{number}
-    while number == " "
+    while number == "X" || number == "O"
       counter += 1
   end
 end
